0d309df53b9ebd6f63407cb131e09b04b658f4f0
Soar1826 <34289582+Soar1826@users.noreply.github.com>
2023-01-24 11:31:17 -0800

    Update admin.js

---
+++ pages/admin.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import Icon from '@hackclub/icons'
+import Modal from '../components/Modal'
+import { useEffect, useState } from 'react';
+
+export default function Admin() {
+    const [modal, setModal] = useState(false);
+  return (
+    <>
+        <Modal visible={modal} setVisible={setModal}>
+            <h1>Login</h1>
+        </Modal>
+    </>
+  );
+}
