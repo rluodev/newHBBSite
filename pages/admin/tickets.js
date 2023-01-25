343e1f53a4a6dfe21da71dd3fd7ace399b062499
Soaringnemesis <34289582+Soar1826@users.noreply.github.com>
2023-01-24 22:31:58 -0800

    fix paths

--- pages/admin/tickets.js
+++ pages/admin/tickets.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../../styles/Home.module.css'
 import Icon from '@hackclub/icons'
-import BigModal from '../components/BigModal'
+import BigModal from '../../components/BigModal'
 import { useEffect, useState } from 'react';
 
 export default function Tickets() {
