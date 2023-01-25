c5c0d74de68ad75690fa6175787c9277c7998f3a
Soar1826 <34289582+Soar1826@users.noreply.github.com>
2023-01-24 16:11:59 -0800

    Update adminLogin.js

--- pages/adminLogin.js
+++ pages/adminLogin.js
@@ -30,7 +30,7 @@ export default function Admin() {
 						alert("Failed to set auth token in local storage. Please make sure cookies are enabled.");
 						console.log("Failed to set auth token in local storage. Please make sure cookies are enabled.");
 					}
-					
+
 				} else {
 					// Else throw an error with the message returned
 					// from the API
@@ -46,33 +46,57 @@ export default function Admin() {
 	}
 	return (
 		<>
-			<Head>
-				<title>Login</title>
-				<meta name="description" content="Login" />
-				<link rel="icon" href="/favicon.ico" />
-				<meta key="og_locale" property="og:locale" content="en_US" />
-				<meta key="og_type" property="og:type" content="website" />
-				<meta key="og_site" property="og:site_name" content="HackBackBetter 2023" />
-				<meta key="og_title" property="og:title" content="HackBackBetter 2023" />
-				<meta key="desc" name="description" content="Login" />
-				<meta key="og_desc" property="og:description" content="Login" />
-				<meta key="tw_desc" name="twitter:description" content="Login" />
-				<meta key="theme_color" name="theme-color" content="purple" />
-				<meta key="tw_card" name="twitter:card" content="summary_large_image" />
-				<meta key="robots" name="robots" content="noindex, nofollow" />
-			</Head>
-			<form>
-				<label>
-					Auth Token:
-					<input
-						type="password"
-						placeholder="Your auth token here..."
-						onChange={e => setToken(e.target.value)}
-					/>
-				</label>
-				<br />
-				<button type="submit" onClick={e => { e.preventDefault(); handleSubmita()}}>Log in</button>
-			</form>
+			<div style={{
+				width: '100vw',
+				height: '100vh',
+				background: 'var(--purple)'
+			}}>
+				<Head>
+					<title>Login</title>
+					<meta name="description" content="Login" />
+					<link rel="icon" href="/favicon.ico" />
+					<meta key="og_locale" property="og:locale" content="en_US" />
+					<meta key="og_type" property="og:type" content="website" />
+					<meta key="og_site" property="og:site_name" content="HackBackBetter 2023" />
+					<meta key="og_title" property="og:title" content="HackBackBetter 2023" />
+					<meta key="desc" name="description" content="Login" />
+					<meta key="og_desc" property="og:description" content="Login" />
+					<meta key="tw_desc" name="twitter:description" content="Login" />
+					<meta key="theme_color" name="theme-color" content="purple" />
+					<meta key="tw_card" name="twitter:card" content="summary_large_image" />
+					<meta key="robots" name="robots" content="noindex, nofollow" />
+				</Head>
+				<Modal visible={true} setVisible={() => ''} hideCloseButton={true}>
+					<div style={{
+						width: '100%',
+						height: '100%',
+						border: 'none',
+						borderRadius: '8px',
+						border: '2px solid var(--purple)',
+						display: 'flex',
+						alignItems: 'center',
+						flexDirection: 'column',
+						justifyContent: 'center',
+						padding: '40px',
+						textAlign: 'center',
+						position: 'relative'
+					}}>
+						<img src="https://cdn.hackbackbetter.live/logo-full-light.png" style={{ maxWidth: '30%', position: 'absolute', bottom: '2rem', right: '2rem' }} />
+						<form>
+							<label>
+								Auth Token:
+								<input
+									type="password"
+									placeholder="Your auth token here..."
+									onChange={e => setToken(e.target.value)}
+								/>
+							</label>
+							<br />
+							<button type="submit" onClick={e => { e.preventDefault(); handleSubmita() }}>Log in</button>
+						</form>
+					</div>
+				</Modal>
+			</div>
 		</>
 	);
 	//update
